7f2f9a80b88fc6678d8ddf19cce5c7926baf9001
Cilyan Olowen <gaknar@gmail.com>
2014-01-06 00:02:21 +0100

    Add support for skins
    Code for displaying skin with the {{skin>Player}} syntax was already
    there but not really functional.
    Now the skin syntax displays a front view of the full player (from head
    to legs).

--- action.php
+++ action.php
@@ -48,8 +48,8 @@ class action_plugin_minecraftskins extends DokuWiki_Action_Plugin {
             exit;
         }
         if ($prefix == '_skin') {
-            if ($size == 0) $size = 250;
-            if ($size < 40) $size = 40;
+            if ($size == 0) $size = 64;
+            if ($size < 16) $size = 16;
             if ($size > $this->GetConf('max_skin_size')) $size = $this->GetConf('max_skin_size');
             $this->_printSkin($player, $size);
             exit;
@@ -91,39 +91,42 @@ class action_plugin_minecraftskins extends DokuWiki_Action_Plugin {
     
     function _printSkin($player, $size) {
         $skin = $this->_getSkin($player);
+        // Define ratio and sizes
+        $dw = $size; // Size is width
+        $dh = $dw*2; // Height is twice width (1:1 makes a character 16x32 px)
+        $r  = $size/16.0; // Ratio of destination
 
-        $g = 4;
-        $p = ($size/100)*5;
-        $s = floor(($size-($p*2))/(48+($g*3)));
-        $p = floor(($size-($s*(48+($g*3))))/2);
-        $h = ($s*32)+($p*2);
-
-        $im = imagecreatefromstring($skin);
-        $mi = imagecreatetruecolor(64,32);
-        imagecopyresampled($mi,$im,0,0,64-1,0,64,32,-64,32);
-        $av = imagecreatetruecolor($size,$h);
-        imagesavealpha($av,true);
-        imagefill($av,0,0,imagecolorallocatealpha($av,0,0,0,127));
+        // Create source image from skin texture
+        $isrc = imagecreatefromstring($skin);
+        // Create mirrored image for left leg and arm
+        $imir = imagecreatetruecolor(64,32);
+        imagecopyresampled($imir,$isrc,0,0,64-1,0,64,32,-64,32);
+        // Alocate target image
+        $idst = imagecreatetruecolor($dw,$dh);
+        imagesavealpha($idst,true);
+        imagefill($idst,0,0,imagecolorallocatealpha($idst,0,0,0,127));
 
         // Front
-        imagecopyresized($av,$im,$p+$s*4,$p,8,8,$s*8,$s*8,8,8);
-        imagecopyresized($av,$im,$p+$s*4,$p+$s*8,20,20,$s*8,$s*12,8,12);
-        imagecopyresized($av,$im,$p,$p+$s*8,44,20,$s*4,$s*12,4,12);
-        imagecopyresized($av,$mi,$p+$s*12,$p+$s*8,16,20,$s*4,$s*12,4,12);
-        imagecopyresized($av,$im,$p+$s*4,$p+$s*8+$s*12,4,20,$s*4,$s*12,4,12);
-        imagecopyresized($av,$mi,$p+$s*8,$p+$s*8+$s*12,56,20,$s*4,$s*12,4,12);
-        
+        // Head
+        imagecopyresized($idst,$isrc,$r*4,0,8,8,$r*8,$r*8,8,8);
+        // Torso
+        imagecopyresized($idst,$isrc,$r*4,$r*8,20,20,$r*8,$r*12,8,12);
+        // Arm right
+        imagecopyresized($idst,$isrc,0,$r*8,44,20,$r*4,$r*12,4,12);
+        // Arm left
+        imagecopyresized($idst,$imir,$r*12,$r*8,16,20,$r*4,$r*12,4,12);
+        // Leg right
+        imagecopyresized($idst,$isrc,$r*4,$r*8+$r*12,4,20,$r*4,$r*12,4,12);
+        // Leg left
+        imagecopyresized($idst,$imir,$r*8,$r*8+$r*12,56,20,$r*4,$r*12,4,12);
         // Accessories
-        imagecopyresized($av,$im,$p+$s*4,$p,40,8,$s*8,$s*8,8,8);
-        imagecopyresized($av,$im,$p+$s*$g+$s*16,$p,32,8,$s*8,$s*8,8,8);
-        imagecopyresized($av,$im,$p+$s*$g*2+$s*28,$p,56,8,$s*8,$s*8,8,8);
-        imagecopyresized($av,$im,$p+$s*$g*3+$s*40,$p,48,8,$s*8,$s*8,8,8);
+        imagecopyresized($idst,$isrc,$r*4,0,40,8,$r*8,$r*8,8,8);
 
         header('Content-type: image/png');
-        imagepng($av);
-        imagedestroy($im);
-        imagedestroy($mi);
-        imagedestroy($av);
+        imagepng($idst);
+        imagedestroy($isrc);
+        imagedestroy($imir);
+        imagedestroy($idst);
     }
 }
 //Setup VIM: ex: et ts=4 :
